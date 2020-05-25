0e6eb12502a429e7984a96188976e634c2994352
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2020-05-26 00:10:14 +0900

    Lint gawk codes

--- build.sh
+++ build.sh
@@ -64,35 +64,36 @@ EOF
 
     grep -E '^@def' "${files[@]}" \
         | sed 's/:/ /' \
-        | gawk -i"$lib" 'BEGIN { FS = " " }
-                           { $1 = basename($1)
-                             # Join fields surrounded by {}
-                             for (i = 3; i <= NF; i++) {
-                                 j = i
-                                 while ( $i ~ /^{/ && $j !~ /}$/ ) {
-                                     j++
-                                     if ( j > NF ) break
-                                 }
-                                 if ( j > i )
-                                     for ( k = i + 1; k <= j; k++ ) {
-                                         $i = $i" "$k
-                                         $k = ""
-                                     }
-                             }
-                             print
-                           }' \
-        | sed -E 's/\t\t+/\t/g' \
         | gawk -i"$lib" \
-              '{ # $3 is either description in @def(fn|tp)x? (e.g. {Class})
-                    # or identifier in @def(mac|spec|fun)x? (e.g. let).
-                    # Some identifiers are surrounded by {} (e.g. {(setter ...)}),
-                    # having () inside.
-                    if ( $3 ~ /^{[^()]+}$/ )
-                        # $3 may have inconsistent cases; e.g. {Condition [tT]ype}
-                        print $1, $2, tolower($3), unwrap($4)
-                    else
-                        print $1, $2, "", unwrap($3)
-                  }' \
+              'BEGIN { FS = " " }
+               { $1 = basename($1)
+                 # Join fields surrounded by {}
+                 for (i = 3; i <= NF; i++) {
+                     j = i
+                     while ($i ~ /^{/ && $j !~ /}$/) {
+                         j++
+                         if (j > NF) break
+                     }
+                     if (j > i)
+                         for (k = i + 1; k <= j; k++) {
+                             $i = $i " " $k
+                             $k = ""
+                         }
+                 }
+                 print
+               }' \
+        | sed -E 's/\t{2,}/\t/g' \
+        | gawk -i"$lib" \
+              '{ # $3 is either category in @def(fn|tp)x? (e.g. {Class}) or
+                 # identifier in @def(mac|spec|fun)x? (e.g. let).
+                 # Some identifiers are surrounded by {} (e.g. {(setter ...)}),
+                 # having () inside.
+                 if ($3 ~ /^{[^()]+}$/)
+                     # $3 may have inconsistent cases; e.g. {Condition [tT]ype}
+                     print $1, $2, tolower($3), unwrap($4)
+                 else
+                     print $1, $2, "", unwrap($3)
+               }' \
         | sort | uniq
 }
 
@@ -109,20 +110,21 @@ EOF
         exit 1
     fi
 
-    gawk -F'\t' '/@defmacx?/ { print $4 }' "$1" \
+    gawk -F'\t' '$2 ~ /^@defmacx?$/ { print $4 }' "$1" \
         | sort | uniq \
         | gawk -i"$lib" '{ print_with_at_expanded($0) }' \
         | find_undefined_keywords_in 'scheme\w*Syntax' \
-        | gawk -F'\t' '{ if ( /^use$/ )
-                            {}  # skip it as it is handled in schemeImport
-                        else if ( /^define-class$/ )
-                            # Can be defined only on toplevel
-                            print "syn keyword schemeSpecialSyntax "$0
-                        else if ( /^\^c$/ )
-                            print "syn match schemeSyntax /\\^[_a-z]/"
-                        else
-                            print "syn keyword schemeSyntax "$0
-                      }'
+        | gawk -F'\t' \
+              '{ if (/^use$/)
+                     {}  # skip it as it is handled in schemeImport
+                 else if (/^define-class$/)
+                     # Can be defined only on toplevel
+                     print "syn keyword schemeSpecialSyntax " $0
+                 else if (/^\^c$/)
+                     print "syn match schemeSyntax /\\^[_a-z]/"
+                 else
+                     print "syn keyword schemeSyntax " $0
+               }'
 }
 
 build_specialform() {
@@ -138,22 +140,23 @@ EOF
         exit 1
     fi
 
-    gawk -F'\t' '/@defspecx?/ { print $4 }' "$1" \
+    gawk -F'\t' '$2 ~ /^@defspecx?$/ { print $4 }' "$1" \
         | sort | uniq \
         | gawk -i"$lib" '{ print_with_at_expanded($0) }' \
         | find_undefined_keywords_in 'scheme\w*Syntax' \
-        | gawk -F'\t' '{ if ( /^import$/ )
-                            {}  # skip it as it is handled in schemeImport
-                        else if ( /^require$/ || \
-                                  /^define-(constant|in-module|inline)$/ )
-                            # Can be defined only on toplevel (except define-inline)
-                            print "syn keyword schemeSpecialSyntax "$0
-                        else if ( /^(define|select)-module$/ || \
-                                  /^export-all$/ )
-                            print "syn keyword schemeLibrarySyntax "$0
-                        else
-                            print "syn keyword schemeSyntax "$0
-                      }'
+        | gawk -F'\t' \
+              '{ if (/^import$/)
+                     {}  # skip it as it is handled in schemeImport
+                 else if (/^require$/ ||
+                          /^define-(constant|in-module|inline)$/)
+                     # Can be defined only on toplevel (except define-inline)
+                     print "syn keyword schemeSpecialSyntax " $0
+                 else if (/^(define|select)-module$/ ||
+                          /^export-all$/)
+                     print "syn keyword schemeLibrarySyntax " $0
+                 else
+                     print "syn keyword schemeSyntax " $0
+               }'
 }
 
 build_function() {
@@ -169,15 +172,14 @@ EOF
         exit 1
     fi
 
-    gawk -F'\t' '/@defunx?/ || \
-                ( /@deftpx?/ && /{function}/ ) || \
-                ( /@deffnx?/ && /{(generic )?function}/ ) {
-                    print $4
-                }' "$1" \
+    gawk -F'\t' \
+        '$2 ~ /^@defunx?$/ ||
+         ($2 ~ /^@deftpx?$/ && $3 ~ /^{function}$/) ||
+         ($2 ~ /^@deffnx?$/ && $3 ~ /^{(generic )?function}$/) { print $4 }' "$1" \
         | sort | uniq \
         | gawk -i"$lib" '{ print_with_at_expanded($0) }' \
         | find_undefined_keywords_in 'schemeFunction' \
-        | gawk -F'\t' '{ print "syn keyword schemeFunction "$0 }'
+        | gawk -F'\t' '{ print "syn keyword schemeFunction " $0 }'
 }
 
 build_variable() {
@@ -193,13 +195,12 @@ EOF
         exit 1
     fi
 
-    gawk -F'\t' '/@defvarx?/ || \
-                /@defvrx?/ && /{comparator}/ {
-                    print $4
-                }' "$1" \
+    gawk -F'\t' \
+        '$2 ~ /^@defvarx?$/ ||
+         ($2 ~ /^@defvrx?$/ && $3 ~ /^{comparator}$/) { print $4 }' "$1" \
         | sort | uniq \
         | gawk -i"$lib" '{ print_with_at_expanded($0) }' \
-        | gawk -F'\t' '{ print "syn keyword schemeVariable "$0 }'
+        | gawk -F'\t' '{ print "syn keyword schemeVariable " $0 }'
 }
 
 build_constant() {
@@ -215,11 +216,11 @@ EOF
         exit 1
     fi
 
-    gawk -F'\t' '/@defvrx?/ && /{constant}/ { print $4 }' "$1" \
+    gawk -F'\t' '$2 ~ /^@defvrx?$/ && $3 ~ /^{constant}$/ { print $4 }' "$1" \
         | sort | uniq \
         | gawk -i"$lib" '{ print_with_at_expanded($0) }' \
         | find_undefined_keywords_in 'schemeConstant' \
-        | gawk -F'\t' '{ print "syn keyword schemeConstant "$0 }'
+        | gawk -F'\t' '{ print "syn keyword schemeConstant " $0 }'
 }
 
 build_module() {
@@ -235,10 +236,11 @@ EOF
         exit 1
     fi
 
-    gawk -F'\t' '/@deftpx?/ && /{(builtin )?module}/ { print $4 }' "$1" \
+    gawk -F'\t' \
+        '$2 ~ /^@deftpx?$/ && $3 ~ /^{(builtin )?module}$/ { print $4 }' "$1" \
         | sort | uniq \
         | gawk -i"$lib" '{ print_with_at_expanded($0) }' \
-        | gawk -F'\t' '{ print "syn keyword gaucheModule "$0 }'
+        | gawk -F'\t' '{ print "syn keyword gaucheModule " $0 }'
 }
 
 build_class() {
@@ -254,10 +256,11 @@ EOF
         exit 1
     fi
 
-    gawk -F'\t' '/@deftpx?/ && /{((builtin )?class|metaclass)}/ { print $4 }' "$1" \
+    gawk -F'\t' \
+        '$2 ~ /^@deftpx?$/ && $3 ~ /^{((builtin )?class|metaclass)}$/ { print $4 }' "$1" \
         | sort | uniq \
         | gawk -i"$lib" '{ print_with_at_expanded($0) }' \
-        | gawk -F'\t' '{ print "syn keyword gaucheClass "$0 }'
+        | gawk -F'\t' '{ print "syn keyword gaucheClass " $0 }'
 }
 
 build_syntax() {
@@ -400,13 +403,13 @@ EOF
 
     local word
     gawk '{ print $4 }' "$@" \
-        | gawk '/^(|r|g)let((|rec)(|1|\*)($|-)|\/)/ || /-let(|rec)(|1|\*)$/ || \
-               /^define($|-)/ || /-define$/ || \
-               /^match($|-)/ || /-match$/ || \
-               /^(|e)case($|-)/ || ( /-(|e)case$/ && $0 !~ /(lower|upper|title)-case$/ ) || \
-               /^lambda($|-)/ || ( /-lambda(|\*)$/ && $0 !~ /^scheme\.case-lambda$/ ) || \
-               /^set!($|-)/ || ( /-set!$/ && $0 !~ /char-set!$/ ) || \
-               /^do(-|times|list)/' \
+        | gawk '/^(|r|g)let((|rec)(|1|\*)($|-)|\/)/ || /-let(|rec)(|1|\*)$/ ||
+                /^define($|-)/ || /-define$/ ||
+                /^match($|-)/ || /-match$/ ||
+                /^(|e)case($|-)/ || ( /-(|e)case$/ && $0 !~ /(lower|upper|title)-case$/ ) ||
+                /^lambda($|-)/ || ( /-lambda(|\*)$/ && $0 !~ /^scheme\.case-lambda$/ ) ||
+                /^set!($|-)/ || ( /-set!$/ && $0 !~ /char-set!$/ ) ||
+                /^do(-|times|list)/' \
         | sort | uniq \
         | while read -r word; do
               if ! grep -F "setl lispwords+=$word" \
@@ -437,101 +440,101 @@ cat > "$lib" <<'EOF'
 BEGIN {
     FS = "\t"
     OFS = "\t"
-    atat[0] = "u8"
-    atat[1] = "s8"
-    atat[2] = "u16"
-    atat[3] = "s16"
-    atat[4] = "u32"
-    atat[5] = "s32"
-    atat[6] = "u64"
-    atat[7] = "s64"
-    atat[8] = "f16"
-    atat[9] = "f32"
-    atat[10] = "f64"
-    atat[11] = "c32"
-    atat[12] = "c64"
-    atat[13] = "c128"
-    html[0] = "a"
-    html[1] = "abbr"
-    html[2] = "acronym"
-    html[3] = "address"
-    html[4] = "area"
-    html[5] = "b"
-    html[6] = "base"
-    html[7] = "bdo"
-    html[8] = "big"
-    html[9] = "blockquote"
-    html[10] = "body"
-    html[11] = "br"
-    html[12] = "button"
-    html[13] = "caption"
-    html[14] = "cite"
-    html[15] = "code"
-    html[16] = "col"
-    html[17] = "colgroup"
-    html[18] = "dd"
-    html[19] = "del"
-    html[20] = "dfn"
-    html[21] = "div"
-    html[22] = "dl"
-    html[23] = "dt"
-    html[24] = "em"
-    html[25] = "fieldset"
-    html[26] = "form"
-    html[27] = "frame"
-    html[28] = "frameset"
-    html[29] = "h1"
-    html[30] = "h2"
-    html[31] = "h3"
-    html[32] = "h4"
-    html[33] = "h5"
-    html[34] = "h6"
-    html[35] = "head"
-    html[36] = "hr"
-    html[37] = "html"
-    html[38] = "i"
-    html[39] = "iframe"
-    html[40] = "img"
-    html[41] = "input"
-    html[42] = "ins"
-    html[43] = "kbd"
-    html[44] = "label"
-    html[45] = "legend"
-    html[46] = "li"
-    html[47] = "link"
-    html[48] = "map"
-    html[49] = "meta"
-    html[50] = "noframes"
-    html[51] = "noscript"
-    html[52] = "object"
-    html[53] = "ol"
-    html[54] = "optgroup"
-    html[55] = "option"
-    html[56] = "p"
-    html[57] = "param"
-    html[58] = "pre"
-    html[59] = "q"
-    html[60] = "samp"
-    html[61] = "script"
-    html[62] = "select"
-    html[63] = "small"
-    html[64] = "span"
-    html[65] = "strong"
-    html[66] = "style"
-    html[67] = "sub"
-    html[68] = "sup"
-    html[69] = "table"
-    html[70] = "tbody"
-    html[71] = "td"
-    html[72] = "textarea"
-    html[73] = "tfoot"
-    html[74] = "th"
-    html[75] = "thead"
-    html[76] = "title"
-    html[77] = "tr"
-    html[78] = "tt"
-    html[79] = "ul"
-    html[80] = "var"
+    ATAT[0] = "u8"
+    ATAT[1] = "s8"
+    ATAT[2] = "u16"
+    ATAT[3] = "s16"
+    ATAT[4] = "u32"
+    ATAT[5] = "s32"
+    ATAT[6] = "u64"
+    ATAT[7] = "s64"
+    ATAT[8] = "f16"
+    ATAT[9] = "f32"
+    ATAT[10] = "f64"
+    ATAT[11] = "c32"
+    ATAT[12] = "c64"
+    ATAT[13] = "c128"
+    HTML[0] = "a"
+    HTML[1] = "abbr"
+    HTML[2] = "acronym"
+    HTML[3] = "address"
+    HTML[4] = "area"
+    HTML[5] = "b"
+    HTML[6] = "base"
+    HTML[7] = "bdo"
+    HTML[8] = "big"
+    HTML[9] = "blockquote"
+    HTML[10] = "body"
+    HTML[11] = "br"
+    HTML[12] = "button"
+    HTML[13] = "caption"
+    HTML[14] = "cite"
+    HTML[15] = "code"
+    HTML[16] = "col"
+    HTML[17] = "colgroup"
+    HTML[18] = "dd"
+    HTML[19] = "del"
+    HTML[20] = "dfn"
+    HTML[21] = "div"
+    HTML[22] = "dl"
+    HTML[23] = "dt"
+    HTML[24] = "em"
+    HTML[25] = "fieldset"
+    HTML[26] = "form"
+    HTML[27] = "frame"
+    HTML[28] = "frameset"
+    HTML[29] = "h1"
+    HTML[30] = "h2"
+    HTML[31] = "h3"
+    HTML[32] = "h4"
+    HTML[33] = "h5"
+    HTML[34] = "h6"
+    HTML[35] = "head"
+    HTML[36] = "hr"
+    HTML[37] = "HTML"
+    HTML[38] = "i"
+    HTML[39] = "iframe"
+    HTML[40] = "img"
+    HTML[41] = "input"
+    HTML[42] = "ins"
+    HTML[43] = "kbd"
+    HTML[44] = "label"
+    HTML[45] = "legend"
+    HTML[46] = "li"
+    HTML[47] = "link"
+    HTML[48] = "map"
+    HTML[49] = "meta"
+    HTML[50] = "noframes"
+    HTML[51] = "noscript"
+    HTML[52] = "object"
+    HTML[53] = "ol"
+    HTML[54] = "optgroup"
+    HTML[55] = "option"
+    HTML[56] = "p"
+    HTML[57] = "param"
+    HTML[58] = "pre"
+    HTML[59] = "q"
+    HTML[60] = "samp"
+    HTML[61] = "script"
+    HTML[62] = "select"
+    HTML[63] = "small"
+    HTML[64] = "span"
+    HTML[65] = "strong"
+    HTML[66] = "style"
+    HTML[67] = "sub"
+    HTML[68] = "sup"
+    HTML[69] = "table"
+    HTML[70] = "tbody"
+    HTML[71] = "td"
+    HTML[72] = "textarea"
+    HTML[73] = "tfoot"
+    HTML[74] = "th"
+    HTML[75] = "thead"
+    HTML[76] = "title"
+    HTML[77] = "tr"
+    HTML[78] = "tt"
+    HTML[79] = "ul"
+    HTML[80] = "var"
 }
 function basename(path,    _path) {
     _path = path
@@ -539,21 +542,21 @@ function basename(path,    _path) {
     return _path
 }
 function unwrap(field,    m) {
-    if ( match(field, /^{\(\w+ (.+)\)}$/, m) )
+    if (match(field, /^{\(\w+ (.+)\)}$/, m))
         return m[1]
     return field
 }
 function print_with_at_expanded(line,    i, _line) {
-    if ( line ~ /@@/ )
-        for (i in atat) {
+    if (line ~ /@@/)
+        for (i in ATAT) {
             _line = line
-            gsub(/@@/, atat[i], _line)
+            gsub(/@@/, ATAT[i], _line)
             print _line
         }
-    else if ( line ~ /html:@var{element}/ )
-        for (i in html) {
+    else if (line ~ /html:@var{element}/)
+        for (i in HTML) {
             _line = line
-            gsub(/@var{element}/, html[i], _line)
+            gsub(/@var{element}/, HTML[i], _line)
             print _line
         }
     else
