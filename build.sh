d2429ecce061a35db4a6d712acdc396fa0c2af3d
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2020-06-02 23:59:30 +0900

    Refactor build.sh

--- build.sh
+++ build.sh
@@ -2,146 +2,12 @@
 
 set -euo pipefail
 
-DATE="$(date +%Y-%m-%d)"
-readonly DATE
-
 TMPD="$(mktemp -d --suffix vimgauche)"
 readonly TMPD
 cleanup() { rm -rf "$TMPD"; }
 trap cleanup ERR SIGTERM EXIT
 
 readonly LIB="$TMPD/lib.awk"
-cat > "$LIB" <<'EOF'
-BEGIN {
-    FS = "\t"
-    OFS = "\t"
-    ATAT[0] = "u8"
-    ATAT[1] = "s8"
-    ATAT[2] = "u16"
-    ATAT[3] = "s16"
-    ATAT[4] = "u32"
-    ATAT[5] = "s32"
-    ATAT[6] = "u64"
-    ATAT[7] = "s64"
-    ATAT[8] = "f16"
-    ATAT[9] = "f32"
-    ATAT[10] = "f64"
-    ATAT[11] = "c32"
-    ATAT[12] = "c64"
-    ATAT[13] = "c128"
-    HTML[0] = "a"
-    HTML[1] = "abbr"
-    HTML[2] = "acronym"
-    HTML[3] = "address"
-    HTML[4] = "area"
-    HTML[5] = "b"
-    HTML[6] = "base"
-    HTML[7] = "bdo"
-    HTML[8] = "big"
-    HTML[9] = "blockquote"
-    HTML[10] = "body"
-    HTML[11] = "br"
-    HTML[12] = "button"
-    HTML[13] = "caption"
-    HTML[14] = "cite"
-    HTML[15] = "code"
-    HTML[16] = "col"
-    HTML[17] = "colgroup"
-    HTML[18] = "dd"
-    HTML[19] = "del"
-    HTML[20] = "dfn"
-    HTML[21] = "div"
-    HTML[22] = "dl"
-    HTML[23] = "dt"
-    HTML[24] = "em"
-    HTML[25] = "fieldset"
-    HTML[26] = "form"
-    HTML[27] = "frame"
-    HTML[28] = "frameset"
-    HTML[29] = "h1"
-    HTML[30] = "h2"
-    HTML[31] = "h3"
-    HTML[32] = "h4"
-    HTML[33] = "h5"
-    HTML[34] = "h6"
-    HTML[35] = "head"
-    HTML[36] = "hr"
-    HTML[37] = "html"
-    HTML[38] = "i"
-    HTML[39] = "iframe"
-    HTML[40] = "img"
-    HTML[41] = "input"
-    HTML[42] = "ins"
-    HTML[43] = "kbd"
-    HTML[44] = "label"
-    HTML[45] = "legend"
-    HTML[46] = "li"
-    HTML[47] = "link"
-    HTML[48] = "map"
-    HTML[49] = "meta"
-    HTML[50] = "noframes"
-    HTML[51] = "noscript"
-    HTML[52] = "object"
-    HTML[53] = "ol"
-    HTML[54] = "optgroup"
-    HTML[55] = "option"
-    HTML[56] = "p"
-    HTML[57] = "param"
-    HTML[58] = "pre"
-    HTML[59] = "q"
-    HTML[60] = "samp"
-    HTML[61] = "script"
-    HTML[62] = "select"
-    HTML[63] = "small"
-    HTML[64] = "span"
-    HTML[65] = "strong"
-    HTML[66] = "style"
-    HTML[67] = "sub"
-    HTML[68] = "sup"
-    HTML[69] = "table"
-    HTML[70] = "tbody"
-    HTML[71] = "td"
-    HTML[72] = "textarea"
-    HTML[73] = "tfoot"
-    HTML[74] = "th"
-    HTML[75] = "thead"
-    HTML[76] = "title"
-    HTML[77] = "tr"
-    HTML[78] = "tt"
-    HTML[79] = "ul"
-    HTML[80] = "var"
-}
-function basename(path) {
-    sub(".*/", "", path)
-    return path
-}
-function unwrap(field,    m) {
-    if (match(field, /^{\(\w+ (.+)\)}$/, m))
-        return m[1]
-    return field
-}
-function print_with_at_expanded(line,    i, _line) {
-    switch (line) {
-    case /@@/:
-        for (i in ATAT) {
-            _line = line
-            gsub(/@@/, ATAT[i], _line)
-            print _line
-        }
-        break
-    case /html:@var{element}/:
-        for (i in HTML) {
-            _line = line
-            gsub(/@var{element}/, HTML[i], _line)
-            print _line
-        }
-        break
-    default:
-        print line
-        break
-    }
-}
-EOF
 
 main() {
     if [[ -z "${GAUCHE_SRC+defined}" ]]; then
@@ -496,7 +362,7 @@ EOF
 }
 
 update_timestamp() {
-    sed -E 's/^(" Last Change:)[0-9]{4}-[0-9]{2}-[0-9]{2}$/\1'"$DATE/"
+    sed -E 's/^(" Last change: )[0-9]{4}-[0-9]{2}-[0-9]{2}$/\1'"$(date +%Y-%m-%d)/"
 }
 
 find_undefined_keywords_in() {
@@ -525,5 +391,137 @@ find_undefined_lispwords() {
 esc() {
     echo "$1" | sed -E 's@(\*|\.|\^|\$)@\\\1@g'
 }
+
+cat > "$LIB" <<'EOF'
+BEGIN {
+    FS = "\t"
+    OFS = "\t"
+    ATAT[0] = "u8"
+    ATAT[1] = "s8"
+    ATAT[2] = "u16"
+    ATAT[3] = "s16"
+    ATAT[4] = "u32"
+    ATAT[5] = "s32"
+    ATAT[6] = "u64"
+    ATAT[7] = "s64"
+    ATAT[8] = "f16"
+    ATAT[9] = "f32"
+    ATAT[10] = "f64"
+    ATAT[11] = "c32"
+    ATAT[12] = "c64"
+    ATAT[13] = "c128"
+    HTML[0] = "a"
+    HTML[1] = "abbr"
+    HTML[2] = "acronym"
+    HTML[3] = "address"
+    HTML[4] = "area"
+    HTML[5] = "b"
+    HTML[6] = "base"
+    HTML[7] = "bdo"
+    HTML[8] = "big"
+    HTML[9] = "blockquote"
+    HTML[10] = "body"
+    HTML[11] = "br"
+    HTML[12] = "button"
+    HTML[13] = "caption"
+    HTML[14] = "cite"
+    HTML[15] = "code"
+    HTML[16] = "col"
+    HTML[17] = "colgroup"
+    HTML[18] = "dd"
+    HTML[19] = "del"
+    HTML[20] = "dfn"
+    HTML[21] = "div"
+    HTML[22] = "dl"
+    HTML[23] = "dt"
+    HTML[24] = "em"
+    HTML[25] = "fieldset"
+    HTML[26] = "form"
+    HTML[27] = "frame"
+    HTML[28] = "frameset"
+    HTML[29] = "h1"
+    HTML[30] = "h2"
+    HTML[31] = "h3"
+    HTML[32] = "h4"
+    HTML[33] = "h5"
+    HTML[34] = "h6"
+    HTML[35] = "head"
+    HTML[36] = "hr"
+    HTML[37] = "html"
+    HTML[38] = "i"
+    HTML[39] = "iframe"
+    HTML[40] = "img"
+    HTML[41] = "input"
+    HTML[42] = "ins"
+    HTML[43] = "kbd"
+    HTML[44] = "label"
+    HTML[45] = "legend"
+    HTML[46] = "li"
+    HTML[47] = "link"
+    HTML[48] = "map"
+    HTML[49] = "meta"
+    HTML[50] = "noframes"
+    HTML[51] = "noscript"
+    HTML[52] = "object"
+    HTML[53] = "ol"
+    HTML[54] = "optgroup"
+    HTML[55] = "option"
+    HTML[56] = "p"
+    HTML[57] = "param"
+    HTML[58] = "pre"
+    HTML[59] = "q"
+    HTML[60] = "samp"
+    HTML[61] = "script"
+    HTML[62] = "select"
+    HTML[63] = "small"
+    HTML[64] = "span"
+    HTML[65] = "strong"
+    HTML[66] = "style"
+    HTML[67] = "sub"
+    HTML[68] = "sup"
+    HTML[69] = "table"
+    HTML[70] = "tbody"
+    HTML[71] = "td"
+    HTML[72] = "textarea"
+    HTML[73] = "tfoot"
+    HTML[74] = "th"
+    HTML[75] = "thead"
+    HTML[76] = "title"
+    HTML[77] = "tr"
+    HTML[78] = "tt"
+    HTML[79] = "ul"
+    HTML[80] = "var"
+}
+function basename(path) {
+    sub(".*/", "", path)
+    return path
+}
+function unwrap(field,    m) {
+    if (match(field, /^{\(\w+ (.+)\)}$/, m))
+        return m[1]
+    return field
+}
+function print_with_at_expanded(line,    i, _line) {
+    switch (line) {
+    case /@@/:
+        for (i in ATAT) {
+            _line = line
+            gsub(/@@/, ATAT[i], _line)
+            print _line
+        }
+        break
+    case /html:@var{element}/:
+        for (i in HTML) {
+            _line = line
+            gsub(/@var{element}/, HTML[i], _line)
+            print _line
+        }
+        break
+    default:
+        print line
+        break
+    }
+}
+EOF
 
 main "$@"
